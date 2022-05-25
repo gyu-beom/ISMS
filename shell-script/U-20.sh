8e2b8fc599f9fb9cdf87371ca992654b1fc86340
gyu-beom <computer.goosebumps96@gmail.com>
2022-05-25 14:02:50 +0900

    #5 U-20.sh sed 명령어 라인 yes -> no 변경.

--- shell-script/U-20.sh
+++ shell-script/U-20.sh
@@ -3,6 +3,6 @@
 cat /etc/xinetd.d/finger | grep disable
 if [ $? -eq 0 ]; then
   LINE=$(egrep -n "(*disable*)" /etc/xinetd.d/finger | cut -d: -f1)
-  sed -e "$LINE s/no/yes/" -i /etc/xinetd.d/finger
+  sed -e "$LINE s/yes/no/" -i /etc/xinetd.d/finger
   service xinetd restart
 fi
